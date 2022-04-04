b40631f8322b9d9557ddd05e6e0e2eeb834978d5
Serj Paskhalov <s.paskhalov@gmail.com>
2022-04-04 16:29:24 +0300

    attention to partially filled docs

--- src/ossDecisionWizard/ossDecisionPaperWizard.ts
+++ src/ossDecisionWizard/ossDecisionPaperWizard.ts
@@ -110,34 +110,50 @@ async function verifyData(ctx: OssHelperContext){
     return
   }
 
-  await verifyDataInner(rowByFlatNum, 'квартиры')
+  let isPreFilled = await verifyDataInner(rowByFlatNum, 'квартиры')
   if(ctx.scene.session?.carPlaces && ctx.scene.session?.carPlaces.length > 0)
-    await verifyDataInner(getRowByCarPlacesNum(), 'м/м')
+    isPreFilled = isPreFilled || await verifyDataInner(getRowByCarPlacesNum(), 'м/м')
   if(ctx.scene.session?.storerooms && ctx.scene.session?.storerooms.length > 0)
-    await verifyDataInner(getRowByCarPlacesNum(), 'кладовки')
+    isPreFilled = isPreFilled || await verifyDataInner(getRowByStoreroomNum(), 'кладовки')
 
-  //const rowByCarPlaces = getRowByCarPlacesNum()
+    if(!isPreFilled){
+      ctx.replyWithMarkdown(`❗️Обратите внимание, что у вас есть *частично заполенные бланки*. В них Вам необходимо заполнить следующие поля:
+- ФИО всех собственников.
+_Например: Иванов Иван Иванович, Иванова Ивана Ивановна_
+- Сведения о документе на право собственности. Номер и дату можно найти в выписке из ЕГРН.
+_Например Номер No 77:09:0001007:13872-77/060/2021-1 от 08.10.2021_
+      `)
+    }
+
+  await ctx.replyWithMarkdown(`При заполнение решений обратите внимание на следующие моменты:
+- Необходимо расписаться внизу каждой страницы.
+- Необходимо подписаться под таблицей на последней странице решения.
+- Поставить галочки в каждой строчке таблицы за/против/воздержался.`);
+  await ctx.replyWithMarkdown(`❗️❗️❗️Распечатайте, заполните и отдайте все документы:❗️❗️❗️
+  - [Александру Шаповалову](https://t.me/alamar1A22). Кв. 819, башня 3, 22-й этаж. Можно принести документы в любой день с 9:00 до 20:00. 
+  - [Леониду](https://t.me/leonid_tj). Свяжитесь заранее через телеграм `)
 
   async function verifyDataInner(
     row: legendData | undefined,
-    objectName: string)
+    objectName: string): Promise<boolean | undefined>
   {    
+    const isPreFilled = row?.owners && row?.owners.length > 0
     let shortOSSBlankPath = './data/short/Empty.pdf'
-    let longOSSBlankPath = './data/long/Empty.pdf'
+    let longOSSBlankPath = './data/long/Empty.pdf'    
   
-    if(row){    
-        if(row.name.length > 0)  
-          await ctx.reply(`Отлично! ${row.name}, я нашел именные бланки для ${objectName}!`)
-        else
-          await ctx.reply(`Отлично! Я нашел именные бланки для ${objectName}!`)
-        shortOSSBlankPath = `./data/short/files/${row.fileNum} _ .pdf`
-        longOSSBlankPath = `./data/long/files/${row.fileNum} _ .pdf`            
+    if(row){
+      const greetingsMsg = row.name.length > 0 ? `${row.name}, я` : 'Я'
+      const blankName = isPreFilled ? 'именные' : 'частично заполненные'
+      await ctx.reply(`Отлично! ${greetingsMsg} нашел ${blankName} бланки для ${objectName}!`)  
+      shortOSSBlankPath = `./data/short/files/${row.fileNum} _ .pdf`
+      longOSSBlankPath = `./data/long/files/${row.fileNum} _ .pdf`
     }else
     {
-      await ctx.reply(`Простите... Но я не нашел именные бланки для ${objectName}:( Я отправлю Вам шаблоны.`)    
+      await ctx.reply(`Простите... Но я не нашел именные бланки для ${objectName}:( Я отправлю Вам шаблоны.`)
     }
   
-    await sendDecisionPapers(ctx, objectName, shortOSSBlankPath, longOSSBlankPath)
+    await sendDecisionPapers(ctx, objectName, shortOSSBlankPath, longOSSBlankPath, isPreFilled)
+    return isPreFilled
   }
 
   function getRowByFlatNum(): legendData | undefined{    
