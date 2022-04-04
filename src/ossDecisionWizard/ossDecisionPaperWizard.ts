a9204039b07a16af8c281355f2320d7a1db8163e
Serj Paskhalov <s.paskhalov@gmail.com>
2022-04-05 00:42:25 +0300

    fixed issue with multiple objects for one owner

--- src/ossDecisionWizard/ossDecisionPaperWizard.ts
+++ src/ossDecisionWizard/ossDecisionPaperWizard.ts
@@ -104,26 +104,32 @@ async function lastStep(ctx: OssHelperContext){
 }
 
 async function verifyDataAndSendDocuments(ctx: OssHelperContext){
-  const rowByFlatNum = getRowByFlatNum()
-  const rowByCarPlacesNum = getRowByCarPlacesNum()
-  const rowByStoreroomNum = getRowByStoreroomNum()
+  const rowsByFlatNum = getRowsByFlatNum()
+  const rowsByCarPlacesNum = getRowsByCarPlacesNum()
+  const rowsByStoreroomNum = getRowsByStoreroomNum()
   if(!checkOwners()){
     await ctx.reply(`Собственники указанные в реестре отличаются от введенных Вами. Попробуйте еще раз.`)
     return
   }
 
-  let isPreFilled = await verifyDataAndSendDocumentsInner(rowByFlatNum, 'квартиры')
-  if(ctx.scene.session?.carPlaces && ctx.scene.session?.carPlaces.length > 0 && rowByFlatNum?.fileNum != rowByCarPlacesNum?.fileNum)
-    isPreFilled = await verifyDataAndSendDocumentsInner(rowByCarPlacesNum, 'м/м') || isPreFilled
-  if(ctx.scene.session?.storerooms && ctx.scene.session?.storerooms.length > 0 && rowByFlatNum?.fileNum != rowByStoreroomNum?.fileNum)
-    isPreFilled = await verifyDataAndSendDocumentsInner(rowByStoreroomNum, 'кладовки') || isPreFilled
+  const rez = await verifyDataAndSendDocumentsInner(rowsByFlatNum, 'квартиры', [])
+  if(ctx.scene.session?.carPlaces && ctx.scene.session?.carPlaces.length > 0){
+    const r = await verifyDataAndSendDocumentsInner(rowsByCarPlacesNum, 'м/м', rez.sendedRows)
+    rez.isPreFilled = r.isPreFilled || rez.isPreFilled
+    rez.sendedRows = rez.sendedRows.concat(r.sendedRows)
+  }
+  if(ctx.scene.session?.storerooms && ctx.scene.session?.storerooms.length > 0){
+    const r = await verifyDataAndSendDocumentsInner(rowsByStoreroomNum, 'кладовки', rez.sendedRows)
+    rez.isPreFilled = r.isPreFilled || rez.isPreFilled
+    rez.sendedRows = rez.sendedRows.concat(r.sendedRows)
+  }
 
   await ctx.replyWithMarkdown(`При заполнение решений обратите внимание на следующие моменты:
 - Необходимо расписаться внизу каждой страницы.
 - Необходимо подписаться под таблицей на последней странице решения.
 - Поставить галочки в каждой строчке таблицы за/против/воздержался.`);
-  if(!isPreFilled){
-    ctx.replyWithMarkdown(`❗️Обратите внимание, что у вас есть *частично заполенные бланки*. В них Вам необходимо дополнительно заполнить следующие поля:
+  if(!rez.isPreFilled){
+    await ctx.replyWithMarkdown(`❗️Обратите внимание, что у вас есть *частично заполенные бланки*. В них Вам необходимо дополнительно заполнить следующие поля:
 - ФИО всех собственников.
 _Например: Иванов Иван Иванович, Иванова Ивана Ивановна_
 - Сведения о документе на право собственности. Номер и дату можно найти в выписке из ЕГРН.
@@ -135,65 +141,83 @@ _Например Номер No 77:09:0001007:13872-77/060/2021-1 от 08.10.202
   - [Леониду](https://t.me/leonid_tj). Свяжитесь заранее через телеграм `)
 
   async function verifyDataAndSendDocumentsInner(
-    row: legendData | undefined,
-    objectName: string): Promise<boolean | undefined>
+    rows: legendData [],
+    objectName: string,
+    alreadySended: number[]): Promise<{ isPreFilled: boolean; sendedRows: number[]; }>
   {    
-    const isPreFilled = row?.owners && row?.owners.length > 0
+    const isPreFilled = rows.some(r => r?.owners && r?.owners.length > 0)  
     let shortOSSBlankPath = `${ctx.dataRoot}/short/Empty.pdf`
     let longOSSBlankPath = `${ctx.dataRoot}/long/Empty.pdf`
+    const name = rows.find(r => r.name.length > 0)?.name
+    const greetingsMsg = name && name.length > 0 ? `${name}, я` : 'Я'
+    const blankName = isPreFilled ? 'именные' : 'частично заполненные'
+    const sendedRows: number [] = []
   
-    if(row){
-      const greetingsMsg = row.name.length > 0 ? `${row.name}, я` : 'Я'
-      const blankName = isPreFilled ? 'именные' : 'частично заполненные'
-      await ctx.reply(`Отлично! ${greetingsMsg} нашел ${blankName} бланки для ${objectName}!`)  
-      shortOSSBlankPath = `${ctx.dataRoot}/short/files/${row.fileNum} _ .pdf`
-      longOSSBlankPath = `${ctx.dataRoot}/long/files/${row.fileNum} _ .pdf`
+    if(rows.length > 0){      
+      await ctx.reply(`Отлично! ${greetingsMsg} нашел ${blankName} бланки для ${objectName}!`)
+      for await (const row of rows) {
+        shortOSSBlankPath = `${ctx.dataRoot}/short/files/${row.fileNum} _ .pdf`
+        longOSSBlankPath = `${ctx.dataRoot}/long/files/${row.fileNum} _ .pdf`  
+        if(!alreadySended.includes(row.fileNum) && !sendedRows.includes(row.fileNum)){
+          await sendDecisionPapers(ctx, objectName, shortOSSBlankPath, longOSSBlankPath, isPreFilled)
+          sendedRows.push(row.fileNum)
+        } 
+      }      
     }else
     {
       await ctx.reply(`Простите... Но я не нашел именные бланки для ${objectName}:( Я отправлю Вам шаблоны.`)
+      await sendDecisionPapers(ctx, objectName, shortOSSBlankPath, longOSSBlankPath, isPreFilled)
     }
-  
-    await sendDecisionPapers(ctx, objectName, shortOSSBlankPath, longOSSBlankPath, isPreFilled)
-    return isPreFilled
+    
+    return {isPreFilled, sendedRows}
   }
 
-  function getRowByFlatNum(): legendData | undefined{ 
+  function getRowsByFlatNum(): legendData []{
+    const rez:legendData[] = [] 
     if(ctx.scene.session?.flats && ctx.scene.session?.flats.length > 0){
       for(let flatNum of ctx.scene.session.flats){    
         const row = ctx.ossLegend.data.find(((d) => d.flats.includes(flatNum)))
         if(row !== undefined)
-          return row
+          rez.push(row)
       }
     }
-    return undefined
+    return rez
   }
 
-  function getRowByCarPlacesNum(): legendData | undefined{   
+  function getRowsByCarPlacesNum(): legendData []{  
+    const rez:legendData[] = []  
     if(ctx.scene.session?.carPlaces && ctx.scene.session?.carPlaces.length > 0){
       for(let carPlace of ctx.scene.session.carPlaces){    
         const row = ctx.ossLegend.data.find(((d) => d.carPlaces.includes(carPlace)))
         if(row !== undefined)
-          return row
+          rez.push(row)
       }
     }
-    return undefined
+    return rez
   }
 
-  function getRowByStoreroomNum(): legendData | undefined{    
+  function getRowsByStoreroomNum(): legendData[]{    
+    const rez:legendData[] = []  
     if(ctx.scene.session?.storerooms && ctx.scene.session?.storerooms.length > 0){
       for(let storeroom of ctx.scene.session.storerooms){    
         const row = ctx.ossLegend.data.find(((d) => d.storerooms.includes(storeroom)))
         if(row !== undefined)
-          return row
+          rez.push(row)
       }
     }
-    return undefined
+    return rez
   }
 
   function checkOwners() {
-    if(!rowByFlatNum?.owners || rowByFlatNum?.owners.length == 0)
-      return true
-    return rowByFlatNum?.owners.every((fio) => ctx.scene.session?.owners.includes(fio))
+    return rowsByFlatNum.every((d) => checkOneOwner(d)) 
+      && rowsByCarPlacesNum.every((d) => checkOneOwner(d))
+      && rowsByStoreroomNum.every((d) => checkOneOwner(d))
+
+    function checkOneOwner(data: legendData){
+      if(!data?.owners || data?.owners.length == 0)
+        return true
+      return data?.owners.every((fio) => ctx.scene.session?.owners.includes(fio))
+    }
   }
 }
 
